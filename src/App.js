789637f84027e6dc211f916400826047a7fb7ea5
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2022-11-03 16:31:34 +0000

    started mobile version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './index.css';
 
-
+import MobileBG from './assets/images/bg-main-mobile.png'
+import Card_Back from './assets/images/bg-card-back.png'
+import Card_Front from './assets/images/bg-card-front.png'
+ 
 
 function App() {
   return (
     <div className="App">
-      <div className="card w-max h-11">
+      <div className="card h-72 w-full bg-center bg-no-repeat bg-cover" style={{backgroundImage: `url(${MobileBG})`}}>
+        <div className="card-img-1 relaltive p-2">
+          <img src={Card_Back} alt=""  className='w-64 absolute left-24 top-14'/>
+          <span className='card-info absolute top-28 right-10 text-white text-sm'>000</span>
+        </div>
+      
 
       </div>
   
